b33af15d07460acae125f4d5ccf947a9a0743eb8
Tim <timeder92@gmail.com>
2023-10-02 15:10:54 +0200

    add pwa config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,50 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' }), vuetify({ autoImport: true })],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon-180x180.png',
+        'maskable-icon-512x512.png',
+        'pwa-64x64.png',
+        'pwa-192x192.png',
+        'pwa-512x512.png'
+      ],
+      manifest: {
+        name: 'Shopping planner PWA',
+        short_name: 'SPPWA',
+        description: 'Get your shopping in order',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    }),
+    vuetify({ autoImport: true })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
